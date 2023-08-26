1553d4ef6973bbce7f00e66c69d02b3424c60398
Temery1213 <temery1213@gmail.com>
2023-08-26 12:04:44 -0500

    fixed blank squares issue
    added progressive transition from white to black

--- etchASketch.js
+++ etchASketch.js
@@ -2,28 +2,42 @@ document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("flexContainer");
     const squareSizeButton = document.getElementById("squareSize");
 
-    function createSquares(totalSquares) {
-        const squareSize = Math.floor(Math.sqrt(10000 / totalSquares)); // Calculate the size for each square
-        const numRows = Math.ceil(100 / squareSize);
-        const numCols = Math.ceil(totalSquares / numRows);
+    // Track the current opacity for each square
+    let currentOpacityMap = new Map();
 
+    function createSquares(totalSquares) {
+        const squareSize = Math.floor(Math.sqrt(10000 / totalSquares));
+        const numCols = Math.floor(100 / squareSize);
+        const numRows = Math.ceil(totalSquares / numCols);
+    
         container.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
-
+    
         for (let i = 0; i < totalSquares; i++) {
             const square = document.createElement("div");
             square.classList.add("squares");
             container.appendChild(square);
 
+            currentOpacityMap.set(square, 0); // Initialize opacity for the square
+    
             square.addEventListener("mouseover", function () {
-                square.style.backgroundColor = "black"; // Change background color on mouseover
+                // Update opacity for the square by 10% each time
+                const currentOpacity = currentOpacityMap.get(square) + 0.1;
+                if (currentOpacity > 1) {
+                    currentOpacityMap.set(square, 1); // Ensure opacity doesn't exceed 1
+                } else {
+                    currentOpacityMap.set(square, currentOpacity);
+                }
+
+                // Set the background color with updated opacity for the square
+                square.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacityMap.get(square)})`;
             });
         }
-
+    
         const squares = document.querySelectorAll(".squares");
         squares.forEach(square => {
-            square.style.flex = `0 0 ${100 / numCols}%`;
-            square.style.paddingBottom = `${100 / numRows}%`;
+            square.style.flex = `0 0 calc(100% / ${numCols})`; 
+            square.style.paddingBottom = `calc(100% / ${numCols})`; 
         });
     }
 
@@ -36,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!isNaN(userInput) && userInput >= 1 && userInput <= 100) {
             // Clear existing squares
             container.innerHTML = "";
+            currentOpacityMap.clear(); // Reset opacity tracking
             createSquares(userInput);
         } else {
             alert("Please enter a valid number between 1 and 100.");
